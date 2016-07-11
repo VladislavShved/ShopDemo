46d0e600bee3a0f60baddf28ee5f6713912736a6
Uladzislau Shved <Uladzislau_Shved@epam.com>
2016-07-11 16:15:28 +0300

    Init project

---
+++ Shop/Shop/Shop.WebUI/App_Start/AutofacConfig.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using Autofac;
+using Autofac.Integration.Mvc;
+using Autofac.Integration.WebApi;
+using Shop.DependencyResolver;
+
+namespace Shop.WebUI
+{
+    public class AutofacCongif
+    {
+        public static void ConfigureContainer()
+        {
+            var builder = new ContainerBuilder();
+
+            // Register dependencies in controllers
+            builder.RegisterControllers(Assembly.GetExecutingAssembly());
+
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
+
+            // Register dependencies in filter attributes
+            builder.RegisterFilterProvider();
+
+            // Register dependencies in custom views
+            builder.RegisterSource(new ViewRegistrationSource());
+
+            // Register our Data dependencies
+            builder.RegisterModule(new DataModule());
+
+            var container = builder.Build();
+            var webApiResolver = new AutofacWebApiDependencyResolver(container);
+            System.Web.Http.GlobalConfiguration.Configuration.DependencyResolver = webApiResolver;
+            // Set MVC DI resolver to use our Autofac container
+            System.Web.Mvc.DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+        }
+    }
+}
